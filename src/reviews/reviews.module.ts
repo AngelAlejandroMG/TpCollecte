2781d6585e549dafac9829f22726fed942c92798
lesvia galvez <2494155@cegepmv.ca>
2026-09-19 13:51:28 -0400

    ajout du service, controller et module reviews

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service.js';
 import { ReviewsController } from './reviews.controller.js';
-
+import { PersistenceModule } from '../persistence/persistence.module.js';
 @Module({
   providers: [ReviewsService],
-  controllers: [ReviewsController]
+  controllers: [ReviewsController],
+  imports: [PersistenceModule],
 })
 export class ReviewsModule {}
